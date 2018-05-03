1293417874b1b4184ed6a9283d7058816456984d
Jason M. Goncalves <jgoncalves@protonmail.com>
2018-05-03 00:19:21 -0400

    Deployed to Heroku

--- config/keys.js
+++ config/keys.js
@@ -6,7 +6,7 @@ module.exports = {
         clientSecret: 'akIDkh0gXjfhyxDYJC9LlYE0'
     },
     mongodb: {
-        dbURI: 'mongodb://jason:jason@ds163689.mlab.com:63689/jason-final-project'
+        dbURI: 'mongodb://root:root@ds113700.mlab.com:13700/heroku_cdd2qgvj'
     },
     session: {
         cookieKey: 'reactisawesome'
